dbb4bb58c1d5a069b2cb9ff41934ded4918b449e
Vitor-Manoel565 <vitoralvesbvgs@gmail.com>
2022-10-26 15:43:36 -0300

    second commit

--- src/api/accessUsers.js
+++ src/api/accessUsers.js
@@ -1,14 +1,22 @@
 import axios from "axios";
 
 export default async function postAcessToken(token){
-  const { data } = await axios.get("https://481d-200-9-19-174.sa.ngrok.io/shadowbanverification", {
-    headers: {
-      Authorization: token,
+    try{
+        const { data } = await axios.get("https://481d-200-9-19-174.sa.ngrok.io/shadowbanverification", {
+          headers: {
+            Authorization: token,
+          }
+        });
+        return {
+          success: true,
+          message: "Token enviado com sucesso",
+          result: data 
+        }
+    }catch(error){
+        return{
+            success: false,
+            message: "Erro ao acessar a API",
+            result: error
+        }
     }
-  });
-  return {
-    success: true,
-    message: "Token enviado com sucesso",
-    result: data 
-  }
 };
