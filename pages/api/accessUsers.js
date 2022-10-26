809b3cee104740e582a3acabd4280c50c46d37fd
Vitor-Manoel565 <vitoralvesbvgs@gmail.com>
2022-10-26 12:42:59 -0300

    add build

--- pages/api/accessUsers.js
+++ pages/api/accessUsers.js
@@ -1,13 +1,13 @@
-import axios from "axios";
+// import axios from "axios";
 
-export default async function postAcessToken(token){
-  const { data } = await axios.post("URL", {
-    headers: {
-      Authorization: token,
-    }
-  });
-  return {
-    success: true,
-    message: "Token enviado com sucesso",
-  }
-};
+// export default async function postAcessToken(token){
+//   const { data } = await axios.post("URL", {
+//     headers: {
+//       Authorization: token,
+//     }
+//   });
+//   return {
+//     success: true,
+//     message: "Token enviado com sucesso",
+//   }
+// };
