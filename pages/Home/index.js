4e5b9de47594806f91b3b10b153957264d54d2f3
Vitor-Manoel565 <vitoralvesbvgs@gmail.com>
2022-10-26 13:32:40 -0300

    first commit

--- pages/Home/index.js
+++ pages/Home/index.js
@@ -1,9 +1,9 @@
 // import FacebookLogin from "react-facebook-login";
 import InstagramLogin from "react-instagram-login";
+import * as s from "../../styles/HomeStyles/index";
 import { useEffect, useState } from "react";
 
-
-export default function App(){
+export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [token, setToken] = useState({});
 
@@ -22,20 +22,36 @@ export default function App(){
   return (
     <div className="App">
       {isLoggedIn === true ? (
-        <></>
-      ) : (
         <div>
-          <h1>Minhas conta está com shadowban?</h1>
-          <h2>Descubra se o alcance do seu perfil está sendo prejudicado por conta do Shadowban.</h2>
-          <InstagramLogin
-            clientId="1296989504384057"
-            buttonText="Login"
-            onSuccess={responseInstagram}
-            onFailure={responseInstagram}
-            scope="user_profile,user_media"
-          />
+          
         </div>
+      ) : (
+        <s.containerHome>
+          <s.containerLogin>
+            <h1>Minhas conta está com shadowban?</h1>
+            <h2>
+              Descubra se o alcance do seu perfil está sendo prejudicado por
+              conta do Shadowban.
+            </h2>
+            <InstagramLogin
+              clientId="1296989504384057"
+              buttonText="Login"
+              onSuccess={responseInstagram}
+              onFailure={responseInstagram}
+              scope="user_profile,user_media"
+            />
+            <div>
+              <h3>Como usar:</h3>
+              <ol>
+                <li>Faça login com sua conta empresarial do instagram.</li>
+                <li>Você precisa ter postado uma foto recente, usando hashtags.</li>
+                <li>Verificamos se o perfil existe e atende aos requisitos.</li>
+                <li>Agora é só descobrir se você está com Shadowban ou não.</li>
+              </ol>
+            </div>
+          </s.containerLogin>
+        </s.containerHome>
       )}
     </div>
   );
-};
+}
---
+++ styles/HomeStyles/index.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const containerHome = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #191c2b;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const containerLogin = styled.div`
+  width: 60%;
+  height: 60%;
+  background-color: #191c2b;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  button{
+    border-radius: 20px;
+  }
+`;
